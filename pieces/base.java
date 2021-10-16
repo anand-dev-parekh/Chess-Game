a18c05b9018a01f780a6a19673d36b7579d55b5e
Anand Parekh <anand.d.parekh@gmail.com>
2021-10-16 16:11:33 -0500

    Made bishop possible move methods
    bishop possible move methods lookin good

--- pieces/base.java
+++ pieces/base.java
@@ -18,10 +18,50 @@ public class base {
         return false;
     }
 
-    public boolean inCheck(base[][] board){
-        return true;
+    public boolean inCheck(base[][] board, int[] newPos){
+        int x = this.position[1];
+        int y = this.position[0];
+
+        int newX = newPos[1];
+        int newY = newPos[0];
+
+
+        board[y][x] = null;
+        board[newY][newX] = this;
+
+        int[] daKing = this.findKing(board);
+
+        if (this.knightCheck(board, daKing)) return true; // NEED TO ADD ALL PIECES TO THIS IF STATEMENT
+
+        return false;
     }
 
+
+    private int[] findKing(base[][] board)
+    {
+        for (int i = 0; i < board.length; i++){
+
+            for (int j = 0; j < board[i].length; j++)
+            {
+                if (board[i][j].piece == "king" && board[i][j].color == this.color){
+                    int[] daKing = {i, j};
+                    return daKing;
+                }
+            }
+        }
+        int[] fake = {0, 1};
+        return fake;
+    }
+
+    private boolean knightCheck(base[][] board, int[] daKing) //STILL NEED TO WRITE FUNCTION
+    {
+
+
+
+        return false;
+
+
+    }
 
 
 }
--- pieces/rook.java
+++ pieces/rook.java
@@ -10,26 +10,9 @@ public class rook extends base{
 
     public boolean validMove(base[][] board, int[] newPos)
     {
-        for (int i = 0; i < board.length; i++)
-        {
-            for (int j = 0; j < board[i].length; j++){
-                
-                if (board[i][j].color == this.color && board[i][j].piece == "king" && !board[i][j].inCheck(board))
-                {
-                    if (this.inMove(board, newPos))return true;
-                    return false;
-                }
-            }
-        }
-        return false;
-    }
-
-    private boolean inMove(base[][] board, int[] newPos)
-    {
         if (this.toRight(board, newPos) || this.toLeft(board, newPos) || this.toTop(board, newPos) || this.toBottom(board, newPos)) return true;
         return false;
     }
-
 
     private boolean toRight(base[][] board, int[] newPos)
     {
@@ -85,9 +68,9 @@ public class rook extends base{
 
         while (y >= 0)
         {
-            int[] topPossible = {y, x};
+            int[] bottomPossible = {y, x};
 
-            if (topPossible == newPos) return true;
+            if (bottomPossible == newPos) return true;
             else if (board[y][x] != null) return false;
             y--;
         }
