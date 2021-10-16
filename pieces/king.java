41318e865ebe9812ab8916cb01cd1f1a6782bbb3
Anand Parekh <anand.d.parekh@gmail.com>
2021-10-16 17:02:55 -0500

    made king validMove() function
    gg ez

--- pieces/king.java
+++ pieces/king.java
@@ -1,4 +1,5 @@
 package pieces;
+import java.lang.Math;
 
 public class king extends base{
 
@@ -9,8 +10,15 @@ public class king extends base{
     }
     
 
-    public boolean inCheck(base[] boardinp)
+    public boolean validMove(base[][] board, int[] newPos)
     {
+        int y = this.position[0], x = this.position[1];
+        int newY = newPos[0], newX = this.position[1];
+
+        int changeY = Math.abs(newY - y),  changeX = Math.abs(newX - x);
+
+        if ((changeY == 0 || changeY == 1) && (changeX == 1 || changeX == 0)) return true;
+
         return false;
     }
 }
