4e65297c272215cf8f9e08223a83440dd5a4f776
Anand Parekh <anand.d.parekh@gmail.com>
2021-10-17 13:16:05 -0500

    Updated Pawn moveValid()

--- src/game/Pawn.java
+++ src/game/Pawn.java
@@ -9,15 +9,19 @@ public class Pawn extends Base{
 
     public boolean validMove(Board board, int newX, int newY) //NEED TO IMPLEMENT EN PEASSANT FIX Pond Color
     {   
+        int decrement = 1;
+        if (this.color == "white"){
+            if (this.x == 6 && newY == this.y - 2 && this.x == newX && board.matrix[newY][newX] == null && board.matrix[this.y - 1][this.x] == null) return true; //CHECKS for jumping twice up
+            decrement = -1;
+        }
+        else{
+            if (this.x == 1 && newY == this.y + 2 && this.x == newX && board.matrix[newY][newX] == null && board.matrix[this.y + 1][this.x] == null) return true; //CHECKS for jumping twice up
+        }
+        
+        if (newY == this.y + decrement && this.x == newX && board.matrix[newY][newX] == null) return true;
 
-
-      
-
-        if (newY == this.y + 2 && this.x == newX && board.matrix[newY][newX] == null && board.matrix[this.y + 1][this.x] == null && this.x == 6) return true; //CHECKS for jumping twice up
-        if (newY == this.y + 1 && this.x == newX && board.matrix[newY][newX] == null) return true;
-
-        if (newY == this.y + 1 && newX == this.x + 1 && board.matrix[newY][newX] != null && board.matrix[newY][newX].color != this.color) return true;
-
+        if (newY == this.y + decrement && newX == this.x - 1 && board.matrix[newY][newX] != null && board.matrix[newY][newX].color != this.color) return true;
+        if (newY == this.y + decrement && newX == this.x + 1 && board.matrix[newY][newX] != null && board.matrix[newY][newX].color != this.color) return true;
 
         return false;
           
